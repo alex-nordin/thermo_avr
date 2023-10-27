c6cd04e9803620334a9a81e86ba6fc0ce7f8131c
Alex Nordin <alex_mc_nordin@proton.me>
2023-10-28 00:24:59 +0200

    Add attempt at printing to lcd

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
 #![no_std]
 #![no_main]
 
+use ag_lcd::*;
 use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let delay = arduino_hal::Delay::new();
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    //data pins
+    let d4 = pins.d4.into_output().downgrade();
+    let d5 = pins.d5.into_output().downgrade();
+    let d6 = pins.d6.into_output().downgrade();
+    let d7 = pins.d7.into_output().downgrade();
 
-    let mut led = pins.d13.into_output();
+    //register select pin
+    let rs = pins.d12.into_output().downgrade();
+    //"enable" pin
+    let en = pins.d11.into_output().downgrade();
 
-    loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
-    }
+    let mut lcd: LcdDisplay<_, _> = LcdDisplay::new(rs, en, delay)
+        .with_half_bus(d4, d5, d6, d7)
+        .with_display(Display::On)
+        .build();
+
+    lcd.print("Test message!");
+
+    loop {}
 }
