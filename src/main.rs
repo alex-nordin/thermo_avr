d3a46f9df4f849e5b174e970f5d779fd6bdccd78
Alex Nordin <alex_mc_nordin@proton.me>
2023-10-29 16:43:11 +0100

    Add comments for clarity

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
+    // initialize access to peripherals as dp according to convention
     let dp = arduino_hal::Peripherals::take().unwrap();
+    // store the pins accessed from dp as a Pins struct
     let pins = arduino_hal::pins!(dp);
+    // init a Delay struct to get easy access to a delay function for our board
     let delay = arduino_hal::Delay::new();
 
     //data pins
@@ -22,10 +25,12 @@ fn main() -> ! {
     let en = pins.d11.into_output().downgrade();
 
     let mut lcd: LcdDisplay<_, _> = LcdDisplay::new(rs, en, delay)
+        // set the display to 4 pin mode usgin "with_half_bus" function
         .with_half_bus(d4, d5, d6, d7)
         .with_display(Display::On)
         .build();
 
+    // inifinite loop. required as hinted from the return type of main
     loop {
         lcd.print("TEST");
     }
