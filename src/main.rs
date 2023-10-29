9f9c260942984295fb387ae0fba4ed3bb27b15e8
Alex Nordin <alex_mc_nordin@proton.me>
2023-10-29 18:14:47 +0100

    Add temp readings

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use ag_lcd::*;
+use embedded_hal::prelude::_embedded_hal_blocking_delay_DelayMs;
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -30,8 +31,26 @@ fn main() -> ! {
         .with_display(Display::On)
         .build();
 
+    // I'm using a KY-013 digital temp sensor, so need to translate an digital reading to analog
+    let mut ad_convert = arduino_hal::Adc::new(dp.ADC, Default::default());
+
+    // temp sensor is connected to the a0 pin
+    let a0 = pins.a0.into_analog_input(&mut ad_convert);
+
     // inifinite loop. required as hinted from the return type of main
     loop {
-        lcd.print("TEST");
+        // clear the screen at the start of each loop
+        lcd.clear();
+        // get temp reading
+        let reading = a0.analog_read(&mut ad_convert);
+        // lcd.print can only take a &str (string slice), so we use the itoa crate to make
+        // a stack allocated buffer to format our u16 int readig into a &str
+        let mut buffer = itoa::Buffer::new();
+        let print = buffer.format(reading);
+
+        lcd.print(print);
+
+        // delay for 5000 ms
+        arduino_hal::delay_ms(5000);
     }
 }
