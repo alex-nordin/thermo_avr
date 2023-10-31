779029c5bd4aa8e04ae33fbeca54ee448b681ccf
Alex Nordin <alex_mc_nordin@proton.me>
2023-10-31 22:41:50 +0100

    Change sensor spec and remove obsolete code

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,9 @@ fn main() -> ! {
     let d6 = pins.d6.into_output().downgrade();
     let d7 = pins.d7.into_output().downgrade();
 
+    //DS12B20 temp module pin
+    let d7 = pins.d7.into_output().downgrade();
+
     //register select pin
     let rs = pins.d12.into_output().downgrade();
     //"enable" pin
@@ -30,36 +33,13 @@ fn main() -> ! {
         .with_display(Display::On)
         .build();
 
-    // I'm using a KY-013 digital temp sensor, so need to translate an digital reading to analog
-    let mut ad_convert = arduino_hal::Adc::new(dp.ADC, Default::default());
-
-    // temp sensor is connected to the a0 pin
-    let a0 = pins.a0.into_analog_input(&mut ad_convert);
-
-    // coefficients for steinhart-hart equation to translate voltage to temp reading
-    let coef_a = 0.001129148;
-    let coef_b = 0.000234125;
-    let coef_c = 0.0000000876741;
-
-    // a value for on board resistance is required for steinhart-hart equation
-    let on_board_r = 10_000f64;
-    //R2 = R1 * (1023.0 / (float)Vo - 1.0)
-
     // inifinite loop. required as hinted from the return type of main
     loop {
         // clear the screen at the start of each loop
         lcd.clear();
         // get temp reading
-        let reading = a0.analog_read(&mut ad_convert);
-        // let num = libm::log(23f64);
-        let resistance = on_board_r * (1023.0f64 / reading as f64 - 1.0f64);
-        let log_of_res = log(resistance.into());
-        // let log_of_res_cube = powf(log_of_res, 3.0);
-        // let tmp_kelvin = 1.0 / coef_a + coef_b * log_of_res + coef_c * log_of_res_cube;
-        // let temp_c = tmp_kelvin - 273.15;
-        // let temper = temp_c as u32;
-
         // lcd.print can only take a &str (string slice), so we use the itoa crate to make
+
         // a stack allocated buffer to format our u16 int readig into a &str
         // let mut buffer = itoa::Buffer::new();
         // let print = buffer.format(temper);
